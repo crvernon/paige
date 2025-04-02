499304c68dbbde097427921d49e00cebb2e5c28a
crvernon <chrisrvernon@gmail.com>
2025-04-02 15:37:17 -0400

    remove fitz dep

--- highlight/utils.py
+++ highlight/utils.py
@@ -10,7 +10,6 @@ from pydantic import BaseModel, Field
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 import requests
-import fitz
 
 import highlight.prompts as prompts
 
